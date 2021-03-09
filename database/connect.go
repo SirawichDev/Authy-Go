a5cc01299cacef11186375bc779e63f4f9cbb211
SirawichDev <sirawit0676@gmail.com>
2021-03-09 10:59:37 +0700

    use .env to keep postgres dns secret

--- database/connect.go
+++ database/connect.go
@@ -2,18 +2,32 @@ package database
 
 import (
 	"demo/models"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
+	"os"
 )
 
+func envFetcher(key string) string {
+	err := godotenv.Load("dev.env")
+	if err != nil {
+		log.Fatalf("Error Cannot load env file")
+	}
+	return os.Getenv(key)
+}
+
 var DB *gorm.DB
+
 func Connect() {
-	dns := "host=localhost user=sirawichvongchuy password=1w2e3r4t5y dbname=auther"
+	dns := "host=" + envFetcher("DB_HOST") + " " + "user=" + envFetcher("DB_USERNAME") + " " + "password=" + envFetcher("DB_PASSWORD") + " " + "dbname=" + envFetcher("DB_NAME")
 	connection, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
 
 	if err != nil {
 		panic("Cloud not connect to database ⚠️")
 	}
 	DB = connection
-	connection.AutoMigrate(&models.User{})
+	if err := connection.AutoMigrate(&models.User{}); err != nil {
+		panic("Cannot migrate to user table")
+	}
 }
