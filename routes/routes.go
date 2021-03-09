6bc12674e2b47e7f6cb4e601682fdbe9b92ad95d
SirawichDev <sirawit0676@gmail.com>
2021-03-09 11:33:02 +0700

    logout route

--- routes/routes.go
+++ routes/routes.go
@@ -5,9 +5,10 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func Setup(app *fiber.App)  {
+func Setup(app *fiber.App) {
 	app.Post("/api/register", controller.Register)
 	app.Post("/api/login", controller.Login)
-	app.Get("/api/user", controller.User)
+	app.Post("/api/logout", controller.Logout)
 
+	app.Get("/api/user", controller.User)
 }
